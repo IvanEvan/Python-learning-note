4229a7e55625b4cab4ba206aabd072af963031c4
Evan <ivanyang93@qq.com>
2018-04-11 09:50:32 +0800

    add range usage

---
+++ range_usage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-1-25 下午3:31
+# @Author  : Evan
+# @Site    : 
+# @File    : range_usage.py
+# @Software: PyCharm Community Edition
+import numpy as np
+for i in range(1, 10, 2):
+    print i
+print np.uint8
