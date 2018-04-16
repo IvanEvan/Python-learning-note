fe20ff721ab98158f2913abe82a515f1a5d9cfcf
Evan <ivanyang93@qq.com>
2018-04-16 11:27:04 +0800

    add qrcode usage

--- qrcode_usage.py
+++ qrcode_usage.py
@@ -4,3 +4,36 @@
 # @Author  : Evan
 # @File    : qrcode_usage.py
 # @Software: PyCharm Community Edition
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=5, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=8, border=4,)
+# img = qrcode.make('Some data here')
+qr.add_data('ahhahahahahaha')
+qr.make(fit=True)
+
+# img = qr.make_image(fill_color='orange', back_color='black')
+img = qr.make_image()
+img = img.convert('RGBA')
+
+icon = Image.open('/home/yn/Pictures/witcher.jpg')
+
+img_w, img_h = img.size
+factor = 4
+size_w = int(img_w / factor)
+size_h = int(img_h / factor)
+
+icon_w, icon_h = icon.size
+if icon_w > size_w:
+    icon_w = size_w
+if icon_h > size_h:
+    icon_h = size_h
+icon = icon.resize((icon_w, icon_h), Image.ANTIALIAS)
+
+w = int((img_w - icon_w)/2)
+h = int((img_h - icon_h)/2)
+icon = icon.convert("RGBA")
+img.paste(icon, (w, h), icon)
+
+img.show()
+img.save('/home/yn/Pictures/test-qr1.png')
